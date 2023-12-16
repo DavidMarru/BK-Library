ba89e9f9d8fa0e26567b0b91c79f75f32098d37e
David Marrucate <144349276+DavidMarru@users.noreply.github.com>
2023-12-16 21:09:27 +0000

    adding appendChild

--- javascrip.js
+++ javascrip.js
@@ -1,31 +1,47 @@
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("dialog + button");
 const closeButton = document.querySelector("dialog button");
-
+const addBookButton = document.querySelector("#addBook");
+const newBookContainer = document.getElementById("newBook");
 
 showButton.addEventListener("click", () => {
     dialog.showModal();
 });
 
-closeButton.addEventListener("click", () =>{
+closeButton.addEventListener("click", () => {
     dialog.close();
 });
 
-function book(title,author,pages,currentPage,read){
-this.title = title;
-this.author = author;
-this.pages = pages;
-this.currentPage = currentPage;
-this.read = function(){
-    if(pages === currentPage){
-        return `Book Completed`
-    } else {
-        return `Not Completed` 
+function Book(title, author, pages, currentPage) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.currentPage = currentPage;
+
+    this.read = function () {
+        return pages === currentPage ? "Book Completed" : "Not Completed";
+    };
+
+    this.createCard = function() {
+        var card = document.createElement("div");
+        card.classList.add("cards");
+        card.innerHTML = `
+            <p>${this.title} by ${this.author},<p>
+            <p>${pages} pages <p>
+            <p>${this.read()}<p>
+        `;
+        newBookContainer.appendChild(card)
     }
-};
-}
+}     
 
-document.querySelector("")
+addBook.addEventListener("click", function () {
+    var title = document.querySelector("#title").value;
+    var author = document.querySelector("#author").value;
+    var pages = document.querySelector("#totalPages").value;
+    var currentPage = document.querySelector("#pagesRead").value;
 
-
+    var newBook = new Book(title, author, pages, currentPage);
+    newBook.createCard();
+    dialog.close();
+});
 
